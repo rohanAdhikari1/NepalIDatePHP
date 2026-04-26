ced1d23ac82c9f287c40a3a7fc0a5a09748565ee
Rohan Adhikari <adhikarirohan249@gmail.com>
2026-04-26 16:47:58 +0545

    Synthesizer base

---
+++ src/Laravel/Livewire/NepaliDateSynth.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RohanAdhikari\NepaliDate\Laravel\Livewire;
+
+use RohanAdhikari\NepaliDate\NepaliDate;
+use RohanAdhikari\NepaliDate\NepaliDateInterface;
+
+class NepaliDateSynth extends Synth
+{
+    public static $key = 'address';
+
+    public static function match($target)
+    {
+        return $target instanceof NepaliDateInterface;
+    }
+
+    public function dehydrate($target)
+    {
+        return [$target->toArray(), []];
+    }
+
+    public function hydrate($value)
+    {
+        $instance = NepaliDate::fromArray($value);
+        return $instance;
+    }
+}
